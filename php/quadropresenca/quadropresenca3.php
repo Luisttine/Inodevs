8cf9a5dbbf910c38cf65b3731f3fd5734a70e344
Gustavo Ando <gustavo.k.ando@gmail.com>
2021-09-27 15:33:52 -0300

    Conexão Quadro de Presença
    Adicionamos a página de quadro de presença e colocamos mecânicas de deletar e editar os dados.

---
+++ php/quadropresenca/quadropresenca3.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/quadro.css">
+     <title>Quadro de Presença</title>
+</head>
+<body>
+
+    <div class="fundo table" >
+        <div class="container" id="hack1">
+          <h2>Quadro de Presença</h2>
+          <div class="menu">
+            <div id="op">
+              <form action=" ">
+                <input type="search"  placeholder="Selecione a alocação" id="pesquisa" onkeyup="pesquisar()"/>
+              </form>
+            </div>
+            <div id="busca">
+              <form action=" ">
+                <input type="search"  placeholder="Busque um nome" id="pesquisa" onkeyup="pesquisar()"/>
+              </form>
+            </div>
+          </div>
+          <div>
+          <div class="quadro" id="hack2">
+                <div class="wrapper">
+                        <ul class="table">
+                            <li class="table__line">
+                                <div class="table__cell table__title table_name one">
+
+                  </div>
+            </div>
+        </div>
+</body>
+</html> 
