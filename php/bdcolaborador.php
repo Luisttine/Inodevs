91daee0c1a67285fd5267f2f7e19abcb3e9558bf
Kauã Gustavo <78959726+Kaua-Reno@users.noreply.github.com>
2021-09-29 13:36:05 -0300

    Ta lá

---
+++ php/bdcolaborador.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+    include_once("conexao.php");
+
+	$matricula = $_POST['matricula'];
+    $cpf = $_POST['cpf'];
+    $nome_completo = $_POST['nome_completo'];
+    $data_admissao = $_POST['data_admissao'];
+    $data_demissao = $_POST['data_demissao'];
+    $funcao = $_POST["funcao"];
+    $situacao = $_POST["situacao"];
+    $tipo_cobertura = $_POST["tipo_cobertura"];
+    $situacaobd = str_replace("ã", "a", "$situacao");
+
+    $result = "INSERT INTO colaboradores (cpf, matricula, nome_completo, data_admissao, data_demissao, funcao, situacao_cadastro, tipo_cobertura) VALUES ('$cpf', '$matricula', '$nome_completo','$data_admissao', '$data_demissao','$funcao', '$situacaobd', '$tipo_cobertura')";
+    $resultado = mysqli_query($conn, $result);
+    
+    if(mysqli_insert_id($conn)){
+        header("Location: colaboradores.php");
+        $_SESSION['msg'] = "<br><p style='color: green; text-align: center'>Colaborador cadastrado com sucesso!</p>";
+    } else {
+        header("Location: colaboradores.php");
+        $_SESSION['msg'] = "<br><p style='color: red; text-align: center'>Erro no Cadastro</p>";
+    }
+?>
