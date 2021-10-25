c0019e03512274bce65d44873cdca86bdf6b8bc5
Luís Souza <63884418+Luisttine@users.noreply.github.com>
2021-10-25 18:40:36 -0300

    Delete php directory

--- php/sair.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['ulogin'],$_SESSION['nome']);
-    $_SESSION['msg'] = "<p style='font-size: 18px; color: green'>Deslogado com sucesso!</p>";
-    header('location: index.php');
-?>
